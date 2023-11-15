89208f256b25c2622e5794798c1fd1f1f0a09f42
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 17:59:59 +0100

    Add project files.

---
+++ pokemonClass/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿namespace pokemonClass
+{
+    class Pokemon
+    {
+        public string Name;
+        public int Lvl;
+        public int Hp;
+
+        public Pokemon(string newName, int newLvl, int newHp)
+        {
+            Name = newName;
+            Lvl = newLvl;
+            Hp = newHp;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nLevel: {Lvl}\nHealth: {Hp}");
+        }
+    }
+}
---
+++ pokemonClass/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace pokemonClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon myPokemon = new Pokemon("Squirtle", 13, 23);
+            myPokemon.showInfo();
+        }
+    }
+}
